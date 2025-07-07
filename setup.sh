6eecf21426422f5f3c8dd3d3950dc2220925e2b5
Daniel Curry <87614608+DanielC5@users.noreply.github.com>
2025-07-06 23:09:08 -0400

    setup script automatically removes config files and creates symlinks respectively

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "WARNING: this script will permanently erase the current config directories for the following programs:"
+
+directories=()
+for dir in "$HOME/dotfiles"/*/; do 
+    name=$(basename "$dir")
+    [ "$name" = "Wallpapers" ] && continue
+    directories+=("$name")
+done
+
+printf "%s\n" "${directories[@]}"
+
+read -p "Are you sure you want to proceed? (y/n)? " user_confirmation
+
+if [[ "$user_confirmation" == "n" || "$user_confirmation" == "N" ]]; then
+    exit 1
+elif [[ "$user_confirmation" == "y" || "$user_confirmation" == "Y" ]]; then
+    for value in "${directories[@]}"; do
+        rm -rf "$HOME/.config/$value"
+        ln -s "$HOME/dotfiles/$value" "$HOME/.config/$value"
+    done
+else
+    echo "Invalid input. Please try again."
+    exit 1
+fi
+
