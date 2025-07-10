bea6346b5b4f59d111458aff0efb8b815cd30f5b
Daniel Curry <87614608+DanielC5@users.noreply.github.com>
2025-07-10 09:26:54 -0400

    removed markview.nvim. no longer conflicts with render-markdown.nvim

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -5,7 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     "MeanderingProgrammer/render-markdown.nvim",
-    "OXY2DEV/markview.nvim",
+    --"OXY2DEV/markview.nvim",
     "echasnovski/mini.diff"
   },
 }
