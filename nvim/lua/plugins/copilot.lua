57969c3778fa36702fe030de8ffc3db081388abc
Daniel Curry <87614608+DanielC5@users.noreply.github.com>
2025-08-31 21:11:36 -0400

    modify copilot.vim shortcuts; can now cycle through suggestions

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,24 +1,20 @@
 return {
   "github/copilot.vim",
   config = function()
-    -- Recommended: disable copilot in certain filetypes
+    -- Disable copilot in certain filetypes
     vim.g.copilot_filetypes = {
       ["TelescopePrompt"] = false,
       ["NvimTree"] = false,
-      ["dap-repl"] = false,
-    }
+      ["dap-repl"] = false}
 
-    -- Optional: Map <Tab> to accept copilot suggestion (fallback to normal tab otherwise)
-    vim.api.nvim_set_keymap(
-      "i",
-      "<Tab>",
-      'copilot#Accept("<CR>")',
-      { silent = true, expr = true, noremap = true }
-    )
+    vim.api.nvim_set_keymap("i","<C-L>",'copilot#Accept("<CR>")',
+      { silent = true, expr = true, noremap = true })
+    vim.api.nvim_set_keymap("i","<C-J>",'copilot#Next()',
+      { silent = true, expr = true, noremap = true })
+    vim.api.nvim_set_keymap("i","<C-K>",'copilot#Previous()',
+      { silent = true, expr = true, noremap = true })
 
-    -- Optional tweaks
     vim.g.copilot_no_tab_map = true    -- prevent default <Tab> mapping
     vim.g.copilot_assume_mapped = true -- allows custom mapping like above
   end,
 }
-
